9cf319fc9466409a884fa37ff16d3dbb6827a159
Matthieu <tieum27@hotmail.com>
2017-06-14 11:13:41 -0700

    grid done

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// Initialize git and after every story add, commit and push your code to github.
 
-// As a user I can see a 10 x 10 grid so that I can see the gameboard.
 
 // As a user when I click on a position, the position changes color so that I can tell that a position has been torpedoed.
 
@@ -10,8 +8,23 @@
 
 
 // *******  Controler ******* //
-$(document).ready(){
+$(document).ready(function(){
+  createTable()
+})
 
-} // end of Doc Ready
+ // end of Doc Ready
 
 // *******  Model ******* //
+
+
+// As a user I can see a 10 x 10 grid so that I can see the gameboard.
+function createTable() {
+  $("#boardGame").append("<table id='boardTable'></table>");
+    for (var i = 0; i < 10; i++) {
+      $("#boardTable").append("<tr id="
+      + i + " class='boardRow'></tr>");
+      for(var j = 0; j < 10; j++) {
+        $("#" + i).append("<td id=" + i + j + " class='boardCell'></td>");
+      } // end for loop to create the columns
+    } // end for loop to create the rows
+} // end of function createTable
