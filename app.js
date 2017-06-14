79d5c10a91054f58aa2568e061ea68a88af21373
Matthieu <tieum27@hotmail.com>
2017-06-14 09:43:53 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Initialize git and after every story add, commit and push your code to github.
+
+// As a user I can see a 10 x 10 grid so that I can see the gameboard.
+
+// As a user when I click on a position, the position changes color so that I can tell that a position has been torpedoed.
+
+// As a user I can see how many torpedoes I have used, so that I can keep track of how many I have used.
+
+// As a user once a position has been torpedoed, it cannot be torpedoed again so that I don't waste torpedoes.
+
+
+// *******  Controler ******* //
+$(document).ready(){
+
+} // end of Doc Ready
+
+// *******  Model ******* //
