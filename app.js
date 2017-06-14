d61bf2e9583844354cc1c0f69898ab5ea1b09894
Matthieu <tieum27@hotmail.com>
2017-06-14 15:29:57 -0700

    torpedo countdown added

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-// As a user when I click on a position I can see if there was a ship at that location so that I can see if I hit a ship. If there is a ship it counts as a hit.
 
 // As a user I have a limit of 25 torpedoes to hit all ships, and when I run out I have lost the game, so that that game is a challenge.
 
@@ -7,24 +6,39 @@
 
 // *******  Controler ******* //
 $(document).ready(function(){
-  createTable()
-  createBoardArray()
-  aiBoats()
-  var clickCount= 0
-  var id
+  createTable();
+  createBoardArray();
+  aiBoats();
+  var clickCount= 0;
+  var hits = 0;
+  var id;
+  var missiles = 25
   // As a user when I click on a position, the position changes color so that I can tell that a position has been torpedoed.
   $("td").on("click",function(){
     id= $(this).attr("id")
-    $("#"+id).css("background-color", "blue")
     var splitId = id.toString().split("");
 
-    // As a user once a position has been torpedoed, it cannot be torpedoed again so that I don't waste torpedoes.
-    if (boardArray[splitId[0]][splitId[1]] === 0){
-      clickCount++; // As a user I can see how many torpedoes I have used, so that I can keep track
-      boardArray[splitId[0]][splitId[1]] = 1; // changes the array to mark it as used
-    } // end of if statement
+    if (missiles >0) {
+      // As a user once a position has been torpedoed, it cannot be torpedoed again so that I don't waste torpedoes.
+      if (boardArray[splitId[0]][splitId[1]] === 2){
+        clickCount++; // As a user I can see how many torpedoes I have used, so that I can keep track
+        $("#"+id).css("background-color", "yellow");
+        // As a user when I click on a position I can see if there was a ship at that location so that I can see if I hit a ship. If there is a ship it counts as a hit.
+        alert("It's a HIT !!!!");
+        hits++;
+      } // end of if statement
+      else if (boardArray[splitId[0]][splitId[1]] === 0) {
+        boardArray[splitId[0]][splitId[1]] = 1; // changes the array to mark it as used
+        $("#"+id).css("background-color", "blue");
+        clickCount++;
+        missiles--;
+      }
+    }
+    else {
+      alert("Game Over!");
+    }
     $("#missilesUsed").text(clickCount)
-
+    $("#hits").text(hits)
 
   })//end of click change color
 })
@@ -68,7 +82,8 @@ function aiBoats() {
         boat = boardArray[Math.floor(Math.random() *9)][Math.floor(Math.random() *9)]
       } // end if statement
       else {
-        boat = 2
+        boardArray[index1][index2] = 2;
+        console.log(boardArray[index1],boat);
         $("#" + index1 + index2).css("background-color", "red");
       }
   } // end for loop
