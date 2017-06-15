6f4f791e246153d27f029c318cbdc1d62f310e30
Mark Cento <markcento@yahoo.com>
2017-06-15 15:44:29 -0700

    added no overlap

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // As a user I don't have ships that touch, so that there is always space between ships.
-// As a user I can torpedo two 3 block ships,, so that the game is diverse.
+
 // As a user I can torpedo two 2 block ships, so that the game is diverse.
 // As a user I can torpedo one 1 block submarine, so that the game is diverse.
 
@@ -10,12 +10,16 @@ $(document).ready(function(){
   createBoardArray();
   airCarrier()
   battleship()
+  // cruiser()
+  // submarine()
+  // detroyer()
+  // mine();
 
-  // aiBoats();
   var clickCount= 0;
   var hits = 0;
   var id;
   var missiles = 25
+
   // As a user when I click on a position, the position changes color so that I can tell that a position has been torpedoed.
   $("td").on("click",function(){
     id= $(this).attr("id")
@@ -80,27 +84,7 @@ function createBoardArray() {
   } // end of second for loop
 }
 
-// making boats
-
-function aiBoats() {
-  for (var b = 0; b < 1; b++){
-    var index1 = Math.floor(Math.random() *9);
-    var index2 = Math.floor(Math.random() *9);
-    var boat = boardArray[index1][index2];
-    console.log(boat);
-    while (boat !== 0){
-        index1 = Math.floor(Math.random() *9);
-        index2 = Math.floor(Math.random() *9);
-        boat = boardArray[index1][index2];
-    } //end of while
-    boatTouch(index1, index2)
-    boardArray[index1][index2] = 2;
-    $("#" + index1 + index2).addClass("boat");
-    // carrier
-
-  } // end for loop
-} // end Function
-
+// **** making boats **** //
 
 // As user I expect there to be 5 single length ships on the board.
 //Carrier creation
@@ -112,6 +96,7 @@ function airCarrier(){
   console.log(vOrH);
   if (vOrH === 0){
     for (var ac = 0; ac < 5; ac++){
+      //boatTouch(index1, (index2 + ac))
       let carrier = 5;
       boardArray[index1][index2 + ac] = carrier;
       $("#" + index1 + (index2 + ac)).addClass("carrier")
@@ -119,10 +104,12 @@ function airCarrier(){
   }
   else {
     for (var ac = 0; ac < 5; ac++){
+      //boatTouch((index1 + ac), (index2))
       let carrier = 5;
       boardArray[index1 + ac][index2] = carrier;
       $("#" + (index1 + ac) + index2).addClass("carrier")
     }
+    boatTouch(index1, index2)
   }
 }//end of Carrier creation
 
@@ -132,35 +119,144 @@ function battleship() {
   for(var z = 0; z < 2; z++){
     var index1 = Math.floor(Math.random() *7);
     var index2 = Math.floor(Math.random() *7);
-    var boat = boardArray[index1][index2];
+    var battleship = boardArray[index1][index2];
     var vOrH = Math.round((Math.random() * 1))
-    console.log(vOrH);
+    while (noOverlap(vOrH, 3, index1, index2) || battleship !== 0){
+        index1 = Math.floor(Math.random() *9);
+        index2 = Math.floor(Math.random() *9);
+        battleship = boardArray[index1][index2];
+    } //end of while
+
     if (vOrH === 0){
       for (var ac = 0; ac < 4; ac++){
-        let carrier = 6;
-        boardArray[index1][index2 + ac] = carrier;
+        //boatTouch(index1, (index2 + ac))
+        let battleship = 6;
+        boardArray[index1][index2 + ac] = battleship;
         $("#" + index1 + (index2 + ac)).addClass("battleship")
       }
     }
     else {
       for (var ac = 0; ac < 4; ac++){
-        let carrier = 6;
-        boardArray[index1 + ac][index2] = carrier;
+        // boatTouch((index1 + ac), index2)
+        let battleship = 6;
+        boardArray[index1 + ac][index2] = battleship;
         $("#" + (index1 + ac) + index2).addClass("battleship")
       }
     }
   }
 }//end of Battleship creation
+// As a user I can torpedo two 3 block ships,, so that the game is diverse.
+//  cruiser
+function cruiser() {
+  for(var z = 0; z < 1; z++){
+    var index1 = Math.floor(Math.random() *8);
+    var index2 = Math.floor(Math.random() *8);
+    var boat = boardArray[index1][index2];
+    var vOrH = Math.round((Math.random() * 1))
+    console.log(vOrH);
 
-//out of bounds checker
-// function boundCheck(i1, i2) {
-//   if (i1 === 0 || i1 === 9 || i2 === 0 || i2 === 9 ) {
-//    aiBoats()
-//  }
-//   // index1 = Math.floor(Math.random() *9);
-//   // index2 = Math.floor(Math.random() *9);
-// }//end of out of bounds check
+//nooverlap = true
+    if (vOrH === 0){
+      for (var ac = 0; ac < 3; ac++){
+        let cruiser = 7;
+        boardArray[index1][index2 + ac] = cruiser;
+        $("#" + index1 + (index2 + ac)).addClass("cruiser")
+      }
+    }
+    else {
+      for (var ac = 0; ac < 3; ac++){
+        let cruiser = 7;
+        boardArray[index1 + ac][index2] = cruiser;
+        $("#" + (index1 + ac) + index2).addClass("cruiser")
+      }
+    }
+  }
+}//end of cruiser creation
+// submarine creating
+function submarine() {
+  for(var z = 0; z < 1; z++){
+    var index1 = Math.floor(Math.random() *8);
+    var index2 = Math.floor(Math.random() *8);
+    var boat = boardArray[index1][index2];
+    var vOrH = Math.round((Math.random() * 1))
+    console.log(vOrH);
+    if (vOrH === 0){
+      for (var ac = 0; ac < 3; ac++){
+        let submarine = 8;
+        boardArray[index1][index2 + ac] = submarine;
+        $("#" + index1 + (index2 + ac)).addClass("submarine")
+      }
+    }
+    else {
+      for (var ac = 0; ac < 4; ac++){
+        let submarine = 8;
+        boardArray[index1 + ac][index2] = submarine;
+        $("#" + (index1 + ac) + index2).addClass("submarine")
+      }
+    }
+  }
+}//end of submarine creation
+// destroyer creating
+function detroyer() {
+  for(var z = 0; z < 2; z++){
+    var index1 = Math.floor(Math.random() *9);
+    var index2 = Math.floor(Math.random() *9);
+    var boat = boardArray[index1][index2];
+    var vOrH = Math.round((Math.random() * 1))
+    console.log(vOrH);
+    if (vOrH === 0){
+      for (var ac = 0; ac < 2; ac++){
+        let detroyer = 9;
+        boardArray[index1][index2 + ac] = detroyer;
+        $("#" + index1 + (index2 + ac)).addClass("detroyer")
+      }
+    }
+    else {
+      for (var ac = 0; ac < 2; ac++){
+        let detroyer = 9;
+        boardArray[index1 + ac][index2] = detroyer;
+        $("#" + (index1 + ac) + index2).addClass("detroyer")
+      }
+    }
+  }
+}//end of detroyer creation
 
+function mine() {
+  for (var b = 0; b < 1; b++){
+    var index1 = Math.floor(Math.random() *9);
+    var index2 = Math.floor(Math.random() *9);
+    var mine = boardArray[index1][index2];
+    while (mine !== 0){
+
+        index1 = Math.floor(Math.random() *9);
+        index2 = Math.floor(Math.random() *9);
+        mine = boardArray[index1][index2];
+    } //end of while
+    boatTouch(index1, index2)
+    boardArray[index1][index2] = 10;
+    $("#" + index1 + index2).addClass("mine");
+  } // end for loop
+} // end mine creation
+
+//overlap function
+function noOverlap (vOrH, length, index1, index2) {
+  var posTaken = true
+  if (vOrH === 0){
+    for (var ac = 0; ac < length; ac++){
+      if (boardArray[index1][index2 + ac] !== 0) {
+        posTaken = false
+      }
+    }
+  }
+  else {
+    for (var ac = 0; ac < length; ac++){
+      if (boardArray[index1 + ac][index2] !== 0) {
+        posTaken = false
+      }
+    }
+  }
+  return posTaken
+} //end overlap function
 //start of checking for touching boats
 function boatTouch(i1, i2) {
   if (i1 === 0 && i2 === 0){
